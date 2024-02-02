1570d88a8361427d4ed46c2b3a5bb2547c4c8242
kashishkumari6677 <aneelkashish@gmail.com>
2024-02-02 19:19:25 +0500

    update

--- 3rdweb.js
+++ 3rdweb.js
@@ -9,3 +9,20 @@ function toggle(){
         Status="hide"
     }
 }
+let body=document.querySelector("body")
+let currMode="light"
+let modeI=document.querySelector('#modeI')
+function changeM(){
+    if(currMode==='light'){
+    body.classList.add('dark')
+    currMode='dark'
+    modeI.src='./media/day-mode.png'
+    console.lo(currMode)
+}else{
+    body.classList.remove('dark')
+    currMode='light'
+    modeI.src='./media/moon.png'
+    console.log(currMode)
+
+}
+}
