19c8b536a52d1d18a47131eac88da5e691b39cbf
kashishkumari6677 <aneelkashish@gmail.com>
2024-02-04 18:19:14 +0500

    update

--- 3rdweb.js
+++ 3rdweb.js
@@ -12,17 +12,23 @@ function toggle(){
 let body=document.querySelector("body")
 let currMode="light"
 let modeI=document.querySelector('#modeI')
+let darkin=document.querySelectorAll("#darkin")
 function changeM(){
     if(currMode==='light'){
     body.classList.add('dark')
+    for(let i=0;i<darkin.length;i++){
+
+        darkin[i].classList.add('dark_toggle')
+    }
     currMode='dark'
     modeI.src='./media/day-mode.png'
-    console.lo(currMode)
 }else{
     body.classList.remove('dark')
+    for(let i=0;i<darkin.length;i++){
+        darkin[i].classList.remove('dark_toggle')
+    }
     currMode='light'
     modeI.src='./media/moon.png'
-    console.log(currMode)
+}
+}
 
-}
-}
